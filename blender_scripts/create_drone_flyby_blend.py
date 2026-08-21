112e6a8c6a29eb8479e4bfe860e4f9c0f6edddc4
GribDev1 <gribdanielv13@gmail.com>
2026-08-21 13:33:59 -0500

    removing rotation on cube

--- blender_scripts/create_drone_flyby_blend.py
+++ blender_scripts/create_drone_flyby_blend.py
@@ -85,7 +85,7 @@ def main():
             name="Cube",
             size=(0.8, 0.8, 0.8),
             location=(0.45, -0.1, -3.6),
-            rotation_deg=(0.0, 20.0, 0.0),
+            rotation_deg=(0.0, 0.0, 0.0),
             color=(0.85, 0.85, 0.85, 1.0),
         )
     )
